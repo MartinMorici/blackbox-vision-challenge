5ce2af969a685fa2e024d7f7d5360e4ff5daed04
Martín Morici <martinmorici3@gmail.com>
2023-03-16 21:14:22 -0300

    challenge casi terminado

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from 'react';
+import './App.scss';
+import Answers from './components/Answers';
+
+
+export interface Question {
+  category: string;
+  type: 'boolean' | 'multiple';
+  difficulty: 'medium' | 'hard' | 'easy';
+  question: string;
+  correct_answer: string;
+  incorrect_answers: string[];
+}
+
+function App() {
+  const [questions, setQuestions] = useState<Question[]>();
+  const [currentQuestion, setCurrentQuestion] = useState<number>(0);
+  const [showResults, setShowResults] = useState<boolean>(false)
+  const [points, setPoints] = useState<number>(0)
+  const [totalPoints, setTotalPoints] = useState(0)
+
+  
+  const getQuestions = async () => {
+    const res = await fetch('https://opentdb.com/api.php?amount=10');
+    const { results } = await res.json();
+    setQuestions(results);
+  };
+  
+  useEffect(() => {
+    getQuestions();
+  }, []);
+  
+  
+  if(!questions){
+    return <div>Loading...</div>
+  }
+  
+  const question = questions[currentQuestion]
+  
+  
+  if(showResults){
+    return <main className='App'>
+      <div className="container">
+        <div className="question">
+          <h1 className='score'>Your score was {points} out of {totalPoints}</h1>
+        </div>
+      </div>
+    </main>
+  }
+
+  return (
+    <main className='App'>
+      <section className='container'>
+        {/* Question */}
+        <article className='question'>
+          {/* Question Header */}
+          <header>
+            <div className='question-info'>
+              <span className='question-number'>
+                Question {currentQuestion+1} / {questions?.length}
+              </span>
+              <span className='question-category'>{question.category}</span>
+            </div>
+            <h1 className='question-text'>{question.question}</h1>
+          </header>
+          
+          <Answers type={question.type} correctAnswer={question.correct_answer} incorrectAnswers={question.incorrect_answers} setPoints={setPoints} currentQuestion={currentQuestion} setCurrentQuestion={setCurrentQuestion} points={points} setShowResults={setShowResults} totalPoints={totalPoints} setTotalPoints={setTotalPoints}/>
+          
+        </article>
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Answers.tsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from 'react';
+import './Answers.scss';
+
+interface Props {
+  type: 'boolean' | 'multiple';
+  correctAnswer: string;
+  incorrectAnswers: string[];
+  currentQuestion: number;
+  points: number;
+  totalPoints: number;
+  setCurrentQuestion: (value: number) => void;
+  setPoints: (value: number) => void;
+  setTotalPoints: (value: number) => void;
+  setShowResults: (value: boolean) => void;
+}
+const Answers = ({ type, correctAnswer, incorrectAnswers, setCurrentQuestion, setPoints, points, currentQuestion, setShowResults, totalPoints, setTotalPoints }: Props) => {
+  const [isCorrect, setIsCorrect] = useState<boolean>();
+  const [isWrong, setIsWrong] = useState<boolean>();
+  const shuffleArray = (array: string[]) => {
+    array.sort(() => Math.random() - 0.5);
+  };
+
+  const handleAnswer = (answer: string) => {
+    if(type === 'multiple'){
+      setTotalPoints(totalPoints + 10)
+    }
+    if(type === 'boolean'){
+      setTotalPoints(totalPoints + 5)
+    }
+
+    if (answer.trim() === correctAnswer.trim()) {
+      setIsCorrect(true);
+      if (type === 'boolean') {
+        setPoints(points + 5);
+      }
+      if (type === 'multiple') {
+        setPoints(points + 10);
+      }
+    } else {
+      setIsWrong(true);
+    }
+  };
+
+  const handleNext = () => {
+    setCurrentQuestion(currentQuestion + 1);
+    setIsCorrect(false);
+    setIsWrong(false);
+  };
+
+  const respuestas = [correctAnswer, ...incorrectAnswers];
+  shuffleArray(respuestas);
+
+  return (
+    <>
+      <main>
+        {type === 'multiple' ? (
+          <div className='respuestas'>
+            {respuestas.map((resp, index) => {
+              return (
+                <div className={`resp ${(isCorrect || isWrong) && 'disable'}`} key={index} onClick={() => {handleAnswer(resp);}}>
+                  {resp}
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <div className='respuestas'>
+            <div className={`resp ${(isCorrect || isWrong) && 'disable'}`} onClick={() => handleAnswer('True')}>
+              True
+            </div>
+            <div className={`resp ${(isCorrect || isWrong) && 'disable'}`} onClick={() => handleAnswer('False')}>
+              False
+            </div>
+          </div>
+        )}
+      </main>
+
+      {isCorrect && (
+        <div className='answer'>
+          <h2 className='correct'>Correct answer!</h2>
+          {currentQuestion !== 9 && (
+            <button className='btn' onClick={() => { handleNext(); }} >
+              Next Question
+            </button>
+          )}
+        </div>
+      )}
+      {isWrong && (
+        <div className='answer'>
+          <h2 className='wrong'>Wrong answer</h2>
+          <h3>The right answer was:</h3>
+          <h3 className='correctAnswer'>{correctAnswer}</h3>
+          {currentQuestion !== 9 && (
+            <button className='btn' onClick={() => { handleNext(); }}>
+              Next Question
+            </button>
+          )}
+        </div>
+      )}
+      {
+        currentQuestion === 9 && (isWrong || isCorrect) && <button className='btn' onClick={() => {setShowResults(true)}}>See Results</button>
+      }
+    </>
+  );
+};
+
+export default Answers;
